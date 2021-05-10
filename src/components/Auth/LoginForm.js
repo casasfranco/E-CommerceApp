c7a165b5b0aaa4822e4c4f0599f562cbddde26ef
Franco Casas <casassfranco@gmail.com>
2021-05-10 19:44:19 -0300

    Controlando y validando datos del formulario de login, section 5, cap 39

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { TextInput, Button } from "react-native-paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import { formStyles } from "../../styles";
 
 export default function LoginForm(props) {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log("Formulario enviado");
+      console.log(formData);
+    },
+  });
   const { changeForm } = props;
   return (
     <View>
-      <TextInput label="Email o Username" style={formStyles.input} />
-      <TextInput label="Contraseña" style={formStyles.input} />
-      <Button mode="contained" style={formStyles.btnSucces}>
+      <TextInput
+        label="Email o Username"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("identifier", text)}
+        value={formik.values.identifier}
+        error={formik.errors.identifier}
+      />
+      <TextInput
+        label="Contraseña"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+        value={formik.values.password}
+        error={formik.errors.password}
+      />
+      <Button
+        mode="contained"
+        style={formStyles.btnSucces}
+        onPress={formik.handleSubmit}
+      >
         Entrar
       </Button>
       <Button
@@ -22,4 +49,18 @@ export default function LoginForm(props) {
       </Button>
     </View>
   );
+}
+
+function initialValues() {
+  return {
+    identifier: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().required(true),
+    password: Yup.string().required(true),
+  };
 }
