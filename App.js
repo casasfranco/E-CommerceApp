61d93063eacf373456ca6e601df70eee10196e11
Franco Casas <casassfranco@gmail.com>
2021-05-10 20:41:32 -0300

    Creando el context Auth, section 5, cap 41

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useMemo, useEffect } from "react";
+import { Text } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import AuthScreen from "./src/screens/Auth";
+import AuthContext from "./src/context/AuthContext";
 
 export default function App() {
   const [auth, setAuth] = useState(undefined);
+
+  useEffect(() => {
+    setAuth(null);
+  }, []);
+
+  //UseMemo sirve para comparar datos y si son distintos actualiza caso contrario nada. Mas optimizada la app
+  const authData = useMemo(
+    () => ({
+      auth,
+      login: () => null,
+      logout: () => null,
+    }),
+    [auth]
+  );
+
+  if (auth === undefined) return null;
+
   return (
-    <PaperProvider>
-      {auth ? <Text>Zona de usuarios</Text> : <AuthScreen />}
-    </PaperProvider>
+    <AuthContext.Provider value={authData}>
+      <PaperProvider>
+        {auth ? <Text>Zona de usuarios</Text> : <AuthScreen />}
+      </PaperProvider>
+    </AuthContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({});
