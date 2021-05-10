4bbf71381702529a023c3bf82ef2bac75eb67738
Franco Casas <casassfranco@gmail.com>
2021-05-10 18:59:19 -0300

    Validando formulario de registro, section 5, cap 35

--- src/components/Auth/RegisterForm.js
+++ src/components/Auth/RegisterForm.js
@@ -10,6 +10,8 @@ export default function RegisterForm(props) {
 
   const formik = useFormik({
     initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    //Si no se valida en la linea anterior, esto no se ejecuta
     onSubmit: (formData) => {
       console.log("Registro de usuario enviado");
       console.log(formData);
@@ -22,18 +24,31 @@ export default function RegisterForm(props) {
         label="Email"
         style={formStyles.input}
         onChangeText={(text) => formik.setFieldValue("email", text)}
+        value={formik.values.email}
+        error={formik.errors.email}
       />
-      <TextInput label="Nombre de usuario" style={formStyles.input} 
-      onChangeText={(text) => formik.setFieldValue("username", text)}
+      <TextInput
+        label="Nombre de usuario"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
+        value={formik.values.username}
+        error={formik.errors.username}
       />
-      <TextInput label="Contraseña" style={formStyles.input} secureTextEntry 
-      onChangeText={(text) => formik.setFieldValue("password", text)}
+      <TextInput
+        label="Contraseña"
+        style={formStyles.input}
+        secureTextEntry
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+        value={formik.values.password}
+        error={formik.errors.password}
       />
       <TextInput
         label="Repetir contraseña"
         style={formStyles.input}
         secureTextEntry
         onChangeText={(text) => formik.setFieldValue("repeatPassword", text)}
+        value={formik.values.repeatPassword}
+        error={formik.errors.repeatPassword}
       />
       <Button
         mode="contained"
@@ -60,5 +75,16 @@ function initialValues() {
     username: "",
     password: "",
     repeatPassword: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string().email(true).required(true), // Tiene que ser del tipo string, email y requerido
+    username: Yup.string().required(true),
+    password: Yup.string().required(true),
+    repeatPassword: Yup.string()
+      .required(true)
+      .oneOf([Yup.ref("password")], true), //.oneOf([Yup.ref('password')], true) controla con password que sean iguales
   };
 }
