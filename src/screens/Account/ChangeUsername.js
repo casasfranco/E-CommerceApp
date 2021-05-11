c3b25dee5988d26967a08ebbf9235b23487bbb03
Franco Casas <casassfranco@gmail.com>
2021-05-11 14:13:41 -0300

    Validando formulario y cargando username, section 7, cap 65. Puede estar diferente.

--- src/screens/Account/ChangeUsername.js
+++ src/screens/Account/ChangeUsername.js
@@ -1,17 +1,79 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { TextInput, Button } from "react-native-paper";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Toast from "react-native-root-toast";
+import { getMeApi, updateUserApi } from "../../api/user";
+import useAuth from "../../hooks/useAuth";
 import { formStyles } from "../../styles";
 
 export default function ChangeUsername() {
+  const [loading, setLoading] = useState(false);
+  const { auth } = useAuth();
+  const navigation = useNavigation();
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        const response = await getMeApi(auth.token);
+        //Si posee username en bd los seteo al formulario como default
+        if (response.username) {
+          await formik.setFieldValue("username", response.username);
+        }
+      })();
+    }, [])
+  );
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      setLoading(true); //Start loading
+      try {
+        await updateUserApi(auth, formData);
+        navigation.goBack();
+      } catch (error) {
+        Toast.show("Error al actualizar los datos.", {
+          position: Toast.positions.CENTER,
+        });
+        setLoading(false); //Stop loading
+      }
+    },
+  });
+
   return (
     <View style={styles.content}>
-      <TextInput label="Nombre de usuario" style={formStyles.input} />
-      <Button mode="contained" style={formStyles.btnSucces}>
+      <TextInput
+        label="Nombre de usuario"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
+        value={formik.values.username}
+        error={formik.errors.username}
+      />
+      <Button
+        mode="contained"
+        style={formStyles.btnSucces}
+        onPress={formik.handleSubmit}
+        loading={loading}
+      >
         Cambiar nombre de usuario
       </Button>
     </View>
   );
+}
+
+function initialValues() {
+  return {
+    username: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required(true),
+  };
 }
 
 const styles = StyleSheet.create({
