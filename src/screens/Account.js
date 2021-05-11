853dfc5060ef77b93317c5ffb8ff16f4d55355e2
Franco Casas <casassfranco@gmail.com>
2021-05-11 08:26:33 -0300

    Obteniendo los datos del usuario, section 7, cap 50

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { ScrollView, Text } from "react-native";
-import StatusBar from '../components/StatusBar'
+import { useFocusEffect } from "@react-navigation/native";
+import StatusBar from "../components/StatusBar";
 import Search from "../components/Search";
-import colors from '../styles/colors'
+import { getMeApi } from "../api/user";
+import useAuth from "../hooks/useAuth";
+import colors from "../styles/colors";
 
 export default function Account() {
+  const [user, setUser] = useState(null);
+  const { auth } = useAuth();
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        const response = await getMeApi(auth.token);
+        console.log(response);
+      })();
+    }, [])
+  );
+
   return (
     <>
-    <StatusBar backgroundColor={colors.bgDark} barStyle='light-content' />
+      <StatusBar backgroundColor={colors.bgDark} barStyle="light-content" />
       <Search />
       <ScrollView>
         <Text>Estamos en la MI CUENTA</Text>
