4afefd23e687c8781e0d633faa85edd99a97bb07
Franco Casas <casassfranco@gmail.com>
2021-05-11 19:02:46 -0300

    Validando formulario y controlando los datos, section 8, cap 75.

--- src/screens/Account/AddAddress.js
+++ src/screens/Account/AddAddress.js
@@ -2,22 +2,70 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { formStyles } from "../../styles";
 
 export default function AddAddress() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log("Creandk dirección...");
+      console.log(formData);
+    },
+  });
+
   return (
     <KeyboardAwareScrollView extraScrollHeight={25}>
       <View style={styles.container}>
         <Text style={styles.title}>Nueva dirección</Text>
-        <TextInput label="Title" style={formStyles.input} />
-        <TextInput label="Nombre y apellidos" style={formStyles.input} />
-        <TextInput label="Dirección" style={formStyles.input} />
-        <TextInput label="Código postal" style={formStyles.input} />
-        <TextInput label="Ciudad" style={formStyles.input} />
-        <TextInput label="Telefono" style={formStyles.input} />
+        <TextInput
+          label="Titulo"
+          style={formStyles.input}
+          onChangeText={(text) => formik.setFieldValue("title", text)}
+          value={formik.values.title}
+          error={formik.errors.title}
+        />
+        <TextInput
+          label="Nombre y apellidos"
+          style={formStyles.input}
+          onChangeText={(text) => formik.setFieldValue("name_lastname", text)}
+          value={formik.values.name_lastname}
+          error={formik.errors.name_lastname}
+        />
+        <TextInput
+          label="Dirección"
+          style={formStyles.input}
+          onChangeText={(text) => formik.setFieldValue("address", text)}
+          value={formik.values.address}
+          error={formik.errors.address}
+        />
+        <TextInput
+          label="Código postal"
+          style={formStyles.input}
+          onChangeText={(text) => formik.setFieldValue("postal_code", text)}
+          value={formik.values.postal_code}
+          error={formik.errors.postal_code}
+        />
+        <TextInput
+          label="Ciudad"
+          style={formStyles.input}
+          onChangeText={(text) => formik.setFieldValue("city", text)}
+          value={formik.values.city}
+          error={formik.errors.city}
+        />
+        <TextInput
+          label="Telefono"
+          style={formStyles.input}
+          onChangeText={(text) => formik.setFieldValue("phone", text)}
+          value={formik.values.phone}
+          error={formik.errors.phone}
+        />
         <Button
           mode="contained"
           style={[formStyles.btnSucces, styles.btnSucces]}
+          onPress={formik.handleSubmit}
         >
           {" "}
           Crear dirección
@@ -25,6 +73,27 @@ export default function AddAddress() {
       </View>
     </KeyboardAwareScrollView>
   );
+}
+
+function initialValues() {
+  return {
+    title: "",
+    name_lastname: "",
+    address: "",
+    postal_code: "",
+    city: "",
+    phone: "",
+  };
+}
+function validationSchema() {
+  return {
+    title: Yup.string().required(true),
+    name_lastname: Yup.string().required(true),
+    address: Yup.string().required(true),
+    postal_code: Yup.string().min(4, true).required(true),
+    city: Yup.string().required(true),
+    phone: Yup.string().min(10, true).required(true),
+  };
 }
 
 const styles = StyleSheet.create({
