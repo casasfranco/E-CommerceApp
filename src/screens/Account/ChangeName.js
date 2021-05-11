d29b98bbec10ec9d96d7f23aa73f944207a007a2
Franco Casas <casassfranco@gmail.com>
2021-05-11 12:49:48 -0300

    , section 7, cap 57

--- src/screens/Account/ChangeName.js
+++ src/screens/Account/ChangeName.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { formStyles } from "../../styles";
 
 export default function ChangeName() {
-    return (
-        <View>
-            <Text>Change name...</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <TextInput label="Nombre" style={formStyles.input} />
+      <TextInput label="Apellidos" style={formStyles.input} />
+      <Button
+        mode="contained"
+        style={formStyles.btnSucces}
+        onPress={() => console.log("Formulario enviado")}
+      >
+        Cambiar nombre y apellidos
+      </Button>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
