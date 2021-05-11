d9a33b3b7378d9758aad74875334abef81ac7caa
Franco Casas <casassfranco@gmail.com>
2021-05-11 16:23:35 -0300

    Actualizando contraseña, section 7, cap 69.

--- src/screens/Account/ChangePassword.js
+++ src/screens/Account/ChangePassword.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { TextInput, Button } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-
+import Toast from "react-native-root-toast";
+import useAuth from "../../hooks/useAuth";
+import { updateUserApi } from "../../api/user";
 import { formStyles } from "../../styles";
 
 export default function ChangePassword() {
+  const { auth } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
+
   const formik = useFormik({
     initialValues: initialValues(),
     validationSchema: Yup.object(validationSchema()),
-    onSubmit: (formData) => {
-      console.log("Formulario enviado");
-      console.log(formData);
+    onSubmit: async (formData) => {
+      setLoading(true);
+      try {
+        const response = await updateUserApi(auth, formData);
+        if (response.statusCode) throw "Error al cambiar la contraseña";
+        navigation.goBack();
+      } catch (error) {
+        Toast.show(error, {
+          position: Toast.positions.CENTER,
+        });
+        setLoading(false);
+      }
     },
   });
 
@@ -38,6 +54,7 @@ export default function ChangePassword() {
         mode="contained"
         style={formStyles.btnSucces}
         onPress={formik.handleSubmit}
+        loading={loading}
       >
         Cambiar contraseña
       </Button>
