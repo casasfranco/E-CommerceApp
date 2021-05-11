d98f5cff1a920bfccf3b243c164e6ae2a9d26f72
Franco Casas <casassfranco@gmail.com>
2021-05-11 13:01:23 -0300

    Integrando formik y yup en el formulario, section 7, cap 58

--- src/screens/Account/ChangeName.js
+++ src/screens/Account/ChangeName.js
@@ -1,22 +1,59 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { TextInput, Button } from "react-native-paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { formStyles } from "../../styles";
 
 export default function ChangeName() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValue) => {
+      console.log("Formulario enviado");
+      console.log(formValue);
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <TextInput label="Nombre" style={formStyles.input} />
-      <TextInput label="Apellidos" style={formStyles.input} />
+      <TextInput
+        label="Nombre"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("name", text)}
+        value={formik.values.name}
+        error={formik.errors.name}
+      />
+      <TextInput
+        label="Apellidos"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("lastname", text)}
+        value={formik.values.lastname}
+        error={formik.errors.lastname}
+      />
       <Button
         mode="contained"
         style={formStyles.btnSucces}
-        onPress={() => console.log("Formulario enviado")}
+        onPress={formik.handleSubmit}
       >
         Cambiar nombre y apellidos
       </Button>
     </View>
   );
+}
+
+function initialValues() {
+  return {
+    name: "",
+    lastname: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    name: Yup.string().required(true),
+    lastname: Yup.string().required(true),
+  };
 }
 
 const styles = StyleSheet.create({
