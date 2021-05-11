353deb401980eaf247fc8f93d1f165191e8b6956
Franco Casas <casassfranco@gmail.com>
2021-05-11 08:42:50 -0300

    Creando el ScreenLoading, section 7, cap 51y52

---
+++ src/screens/Account/Account.js
@@ -0,0 +1,40 @@
+import React, { useState, useCallback } from "react";
+import { ScrollView, Text } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import StatusBar from "../../components/StatusBar";
+import Search from "../../components/Search";
+import ScreenLoading from "../../components/ScreenLoading";
+import { getMeApi } from "../../api/user";
+import useAuth from "../../hooks/useAuth";
+import colors from "../../styles/colors";
+
+export default function Account() {
+  const [user, setUser] = useState(null);
+  const { auth } = useAuth();
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        const response = await getMeApi(auth.token);
+        setUser(response);
+      })();
+    }, [])
+  );
+
+  return (
+    <>
+      <StatusBar backgroundColor={colors.bgDark} barStyle="light-content" />
+
+      {!user ? (
+        <ScreenLoading size="large" text="Hola..." />
+      ) : (
+        <>
+          <Search />
+          <ScrollView>
+            <Text>Estamos en la MI CUENTA</Text>
+          </ScrollView>
+        </>
+      )}
+    </>
+  );
+}
