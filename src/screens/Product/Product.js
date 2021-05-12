d59260263c730a9444b7ac02964bd622238fce67
Franco Casas <casassfranco@gmail.com>
2021-05-12 09:59:16 -0300

    Sistema de loading, section 9, cap 90.

--- src/screens/Product/Product.js
+++ src/screens/Product/Product.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import StatusBar from "../../components/StatusBar";
+import Search from "../../components/Search";
+import ScreenLoading from "../../components/ScreenLoading";
 import { getProductApi } from "../../api/product";
+import colors from "../../styles/colors";
 
 export default function Product(props) {
   const { route } = props;
@@ -17,8 +21,18 @@ export default function Product(props) {
   }, [params]);
 
   return (
-    <View>
-      <Text>{params.idProduct}</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={colors.bgDark} barstyle="light-content" />
+      <Search />
+      {product ? (
+        <ScreenLoading text="Cargando producto" size="large" />
+      ) : (
+        <View>
+          <Text>Hola</Text>
+        </View>
+      )}
+    </>
   );
 }
+
+const styles = StyleSheet.create({});
