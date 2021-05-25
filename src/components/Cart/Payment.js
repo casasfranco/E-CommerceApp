5b514f09d0b3e7df060c71621140e4e1dc2e530c
Franco Casas <casassfranco@gmail.com>
2021-05-25 19:38:30 -0300

    Controlado los datos del, section 13, cap 149.

--- src/components/Cart/Payment.js
+++ src/components/Cart/Payment.js
@@ -1,28 +1,69 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { TextInput, Button } from "react-native-paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { formStyles } from "../../styles";
 import colors from "../../styles/colors";
 
 export default function Payment(props) {
   const { products, selectedAddress, totalPayment } = props;
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      console.log("Realizando pago");
+      console.log(formData);
+    },
+  });
   return (
     <View style={styles.container}>
       <Text style={styles.containerTitle}>Forma de pago</Text>
-      <TextInput label="Nombre de la tarjeta" style={formStyles.input} />
-      <TextInput label="Numero de la tarjeta" style={formStyles.input} />
+      <TextInput
+        label="Nombre de la tarjeta"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("name", text)}
+        value={formik.values.name}
+        error={formik.errors.name}
+      />
+      <TextInput
+        label="Numero de la tarjeta"
+        style={formStyles.input}
+        onChangeText={(text) => formik.setFieldValue("number", text)}
+        value={formik.values.number}
+        error={formik.errors.number}
+      />
       <View style={styles.containerInputs}>
         <View style={styles.containerMonthYearInputs}>
-          <TextInput label="Mes" style={styles.inputDate} />
-          <TextInput label="Año" style={styles.inputDate} />
+          <TextInput
+            label="Mes"
+            style={styles.inputDate}
+            onChangeText={(text) => formik.setFieldValue("exp_month", text)}
+            value={formik.values.exp_month}
+            error={formik.errors.exp_month}
+          />
+          <TextInput
+            label="Año"
+            style={styles.inputDate}
+            onChangeText={(text) => formik.setFieldValue("exp_year", text)}
+            value={formik.values.exp_year}
+            error={formik.errors.exp_year}
+          />
         </View>
-        <TextInput label="CVV/CVC" style={styles.inputCVC} />
+        <TextInput
+          label="CVV/CVC"
+          style={styles.inputCVC}
+          onChangeText={(text) => formik.setFieldValue("cvc", text)}
+          value={formik.values.cvc}
+          error={formik.errors.cvc}
+        />
       </View>
 
       <Button
         mode="contained"
         contentStyle={styles.btnContent}
         labelStyle={styles.btnText}
+        onPress={formik.handleSubmit}
       >
         Pagar {totalPayment && `($${totalPayment})`}
       </Button>
@@ -30,11 +71,30 @@ export default function Payment(props) {
   );
 }
 
+function initialValues() {
+  return {
+    number: "",
+    exp_month: "",
+    exp_year: "",
+    cvc: "",
+    name: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    number: Yup.string().min(16, true).max(16, true).required(true),
+    exp_month: Yup.string().min(1, true).max(2, true).required(true),
+    exp_year: Yup.string().min(2, true).max(2, true).required(true),
+    cvc: Yup.string().min(3, true).max(3, true).required(true),
+    name: Yup.string().min(4, true).required(true),
+  };
+}
+
 const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     marginBottom: 30,
-    
   },
   containerTitle: {
     paddingBottom: 10,
