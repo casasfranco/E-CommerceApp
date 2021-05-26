8bae60156ae100ebb50681fabe57b948aacc009a
Franco Casas <casassfranco@gmail.com>
2021-05-26 16:39:13 -0300

    Obteniendo todos los pedidos del usuario, section 15, cap 160.

--- src/screens/Account/Orders.js
+++ src/screens/Account/Orders.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ScrollView, Text } from "react-native";
 import StatusBar from "../../components/StatusBar";
+import useAuth from "../../hooks/useAuth";
+import { getOrdersApi } from "../../api/order";
 
 export default function Orders() {
+  const [orders, setOrders] = useState(null);
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getOrdersApi(auth);
+      console.log(response);
+    })();
+  }, []);
+
   return (
     <>
       <StatusBar />
